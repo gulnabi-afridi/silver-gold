c71eb5ae23a57feec969397e2a9a24cbebb711f9
gulnabi-afridi <mac@macs-MacBook-Pro.local>
2023-04-02 16:42:55 +0500

    text-bugs-fixed

--- src/component/Roadmap/Roadmap.tsx
+++ src/component/Roadmap/Roadmap.tsx
@@ -58,9 +58,9 @@ function Roadmap() {
             You will find a List of Everything We Offer Bellow
           </p>
           <p className={styles.weOfferText}>
-            However, if you are intrested in something that you do not see
-            listed, let us know and we can try get it for you. <br /> To get
-            started, just pick up the phone and call Toll-Free 1-800-604-0240.{" "}
+            However if you are intrested in something that you do not see
+            listed let us know and we can try get it for you. <br /> To get
+            started just pick up the phone and call Toll-Free 1-800-604-0240.{" "}
           </p>
         </div>
       </div>
@@ -75,19 +75,19 @@ const roadmapData = [
     number: "1",
     img: "/Assets/lock.png",
     title: "Open Your Account",
-    text: "Call us Toll-Free 1-800-604-0240 to let us know you are ready to open an account. we will email your standard customer agreement Review it, Sign it and send it back and you will be ready to work with a precious Metals Advisor to fund your account and start investing",
+    text: "Call us Toll-Free 1-800-604-0240 to let us know you are ready to open an account. we will email your standard customer agreement Review it Sign it and send it back and you will be ready to work with a precious Metals Advisor to fund your account and start investing",
   },
   {
     number: "2",
     img: "/Assets/coin.png",
     title: "Open Your Account",
-    text: "  Motivation also involves factors that direct and maintaingoal-directed actions. Although, such motives are rarelydirectly observable. As a result, we must often infer the reasons why people do the things that they do based on observable behaviors ",
+    text: "  Motivation also involves factors that direct and maintaingoal-directed actions. Although, such motives are rarelydirectly observable. As a result we must often infer the reasons why people do the things that they do based on observable behaviors ",
   },
   {
     number: "3",
     img: "/Assets/box.png",
     title: "Choce precious metal",
-    text: "  Motivation also involves factors that direct and maintaingoal-directed actions. Although, such motives are rarelydirectly observable. As a result, we must often infer the reasons why people do the things that they do based on observable behaviors ",
+    text: "  Motivation also involves factors that direct and maintaingoal-directed actions. Although, such motives are rarelydirectly observable. As a result we must often infer the reasons why people do the things that they do based on observable behaviors ",
   },
 ];
 
